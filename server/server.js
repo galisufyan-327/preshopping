6b6c2ef4d04d7270b655bbebfe572f290ec5012e
Ali Sufyan <alisufyan327@gmail.com>
2018-08-29 20:28:45 +0500

    Add POST /product route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,22 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.post('/product', (req, res) => {
+    var product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        price_old: req.body.price_old,
+        sale_percent: req.body.sale_percent,
+        key_features: req.body.key_features,
+        image_url: req.body.image_url
+    });
+    product.save().then((doc) => {
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started up server on ${port}`);
 });
