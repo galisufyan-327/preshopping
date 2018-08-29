9d596b9c3fc9dddb5415702fd0feaf21121b57ea
Ali Sufyan <alisufyan327@gmail.com>
2018-08-28 18:14:18 -0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Started up server on ${port}`);
+});
